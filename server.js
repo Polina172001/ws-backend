7eeffb84c1c8de57e09fb9e8383e38ba38fed27d
Polina Filippova <polina17012001@mail.ru>
2021-10-05 23:54:42 +0300

    version1

---
+++ server.js
@@ -0,0 +1,139 @@
+const http = require('http');
+const Koa = require('koa');
+const cors = require('@koa/cors');
+const koaBody = require('koa-body');
+const WS = require('ws');
+
+const app = new Koa();
+
+app.use(cors());
+
+app.use(koaBody({
+  urlencoded: true,
+  multipart: true,
+  json: true,
+}));
+
+const port = process.env.PORT || 3002;
+const server = http.createServer(app.callback());
+const wsServer = new WS.Server({
+  server
+});
+
+const messages = [{
+  userID: 1,
+  nicName: 'Misha',
+  text: 'Some text',
+  time: getCurrentTime()
+}];
+const users = new Map();
+
+users.set('1', 'Misha');
+
+
+wsServer.on('connection', (ws) => {
+  let id = 0;
+  const errCallback = (e) => {
+    console.log(e);
+  }
+
+  ws.on('messange', (e) => {
+    if (e === 'allData') {
+      let arrayMap = Array.from(users.entries());
+
+      ws.send(JSON.stringify({
+        message: messages,
+        userList: arrayMap
+      }), errCallback);
+      return;
+    }
+
+    if (JSON.parse(e).hasOwnProperty('checkUser')) {
+      const user = (JSON.parse(e)).checkUser;
+      const userID = (JSON.parse(e)).userID;
+
+      if (messages.some((e) => {
+          e.nicName === user
+        })) {
+        ws.send(JSON.stringify({
+          hasUser: 'exist'
+        }));
+      } else {
+        messages.push({
+          userID,
+          nicName: user,
+          text: `${user} присоединиться к чату `,
+          time: getCurrentTime()
+        });
+        users.set(userID, user);
+        id = userID;
+
+        ws.send(JSON.stringify({
+          hasUser: 'not-exist'
+        }));
+
+        let arrayMap = Array.from(users.entries());
+
+        Array.from(wsSerever.clients)
+          .filter(client => client.readyState === WS.OPEN)
+          .forEach(client => client.send(JSON.stringify({
+            message: [{
+              userID,
+              nicName: user,
+              text: `${user} присоединиться к чату `,
+              time: getCurrentTime()
+            }],
+            userList: arrayMap
+          }), errCallback));
+
+      }
+      return;
+    }
+
+    messages.push(JSON.parse(e));
+
+    let arrayMap = Array.from(users.entries());
+
+    Array.from(wsSerever.clients)
+      .filter(client => client.readyState === WS.OPEN)
+      .forEach(client => client.send(JSON.stringify({
+        message: e,
+        userList: arrayMap
+      }), errCallback));
+  })
+
+  ws.on('close', e => {
+    users.delete(id);
+    let arrayMap = Array.from(users.entries());
+    Array.from(wsSerever.clients)
+      .filter(client => client.readyState === WS.OPEN)
+      .forEach(client => client.send(JSON.stringify({
+        subject: 'Пользователь ушел',
+        userList: arrayMap
+      })));
+  })
+
+
+});
+
+server.listen(port);
+
+function getCurrentTime() {
+  let now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth() + 1;
+  if (month < 10) {
+    month = 0 + '' + month;
+  }
+  let day = now.getDate();
+  let hour = now.getHours();
+  if (hour < 10) {
+    hour = 0 + '' + hour;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = 0 + '' + minutes;
+  }
+
+  return day + '.' + month + '.' + year + '' + hour + ':' + minutes;
+}
